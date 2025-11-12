e0c361265fa91358090e35af6c22e6ee04bba198
WISSTACK <71902497+igorwisniewski@users.noreply.github.com>
2025-11-12 09:05:25 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import { createClient } from '@/utils/supabase/server'
-import { redirect } from 'next/navigation'
+
 import LoginHome from "@/components/LoginHome";
 
 export default async function HomePage() {
-  const supabase = createClient()
 
-  // Sprawdzamy, czy jest zalogowany użytkownik (po stronie serwera)
-  const { data: { user } } = await supabase.auth.getUser()
-
-  // Jeśli użytkownik JEST zalogowany...
-  if (user) {
-    // ...przekieruj go natychmiast na /kontakty
-    // (Zauważ, że zmieniam na '/kontakty', aby pasowało do Twojego formularza)
-    redirect('/kontakty')
-  }
 
   // Jeśli użytkownik NIE jest zalogowany...
   // ...renderuj komponent z formularzem logowania
